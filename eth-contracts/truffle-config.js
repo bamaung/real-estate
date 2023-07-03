53ff2e261e9e2e68c6c34dac30536851f12e6737
Ye Myat Oo <yemyatoo@connectcentregroup.com>
2023-07-03 12:14:01 +0800

    update truffle-config.js

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -1,9 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const homedir = require('os').homedir();
+const INFURA_KEY = fs.readFileSync(homedir + "/.dev/infura_key").toString().trim();
+const MNEMONIC = fs.readFileSync(homedir + "/.dev/mnemonic").toString().trim();
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*",
+        },
+        goerli: {
+            provider: function() {
+                return new HDWalletProvider(MNEMONIC,`https://goerli.infura.io/v3/${INFURA_KEY}`)
+            },
+            network_id: "5"
+        },
+        sepolia: {
+            provider: function() {
+                return new HDWalletProvider(MNEMONIC,`https://sepolia.infura.io/v3/${INFURA_KEY}`)
+            },
+            network_id: "11155111"
         }
     },
     compilers: {
